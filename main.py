9127abae3c03ce134a80f156ba1f1a2e1c86392c
Stefan Bullones <stefan.bullones@gmail.com>
2024-11-15 23:32:59 +0100

    add prometheus metrics

--- main.py
+++ main.py
@@ -2,18 +2,54 @@ import os
 import discord
 import aiohttp
 from discord.ext import commands
+from discord.ext.prometheus import PrometheusCog
+from prometheus_client import start_http_server, Gauge
 from dotenv import load_dotenv
 
 load_dotenv()
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True
+intents.presences = True  # Required for online status tracking
+
+# Start the Prometheus metrics server
+start_http_server(8000)
 
 bot = commands.Bot(command_prefix='!', intents=intents)
+
+# Define Prometheus metrics
+total_users = Gauge('discord_total_users', 'Total number of users registered in the Discord server')
+online_users = Gauge('discord_online_users', 'Total number of online users in the Discord server')
+
+async def update_metrics():
+    """Update Prometheus metrics with current Discord stats"""
+    for guild in bot.guilds:
+        # Update total users metric
+        total_users.set(len(guild.members))
+        # Update online users metric (includes online, idle, and dnd statuses)
+        online_count = len([m for m in guild.members if m.status != discord.Status.offline])
+        online_users.set(online_count)
 
 @bot.event
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
+    # Add the Prometheus cog
+    await bot.add_cog(PrometheusCog(bot))
+    # Initial metrics update
+    await update_metrics()
+
+@bot.event
+async def on_member_join(member):
+    await update_metrics()
+
+@bot.event
+async def on_member_remove(member):
+    await update_metrics()
+
+@bot.event
+async def on_presence_update(before, after):
+    await update_metrics()
 
 @bot.command(name='unexpo')
 async def unexpo(ctx):
